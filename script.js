29052caa36eb04cc1994b3d0eb45ef1265124c97
Yashpriyadeep Katta <yashpriyadeepkatta@Yashpriyadeeps-MacBook-Air.local>
2025-04-15 11:12:59 +0530

    an alternate

--- script.js
+++ script.js
@@ -1,336 +1,120 @@
-let player = [], flag = 0;
-player[0] = false;
-player[1] = false;
+let player = [];
+player[0] = '<i class="fa-solid fa-xmark fa-7.5x"></i>';
+player[1] = '<i class="fa-solid fa-o fa-7x"></i>';
 
-let lock = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-];
+let squares = document.getElementsByClassName('sqr');
+let currentPlayer = player[0];
 
-let element_00 = document.querySelector("#sqr_00");
-let element_01 = document.querySelector("#sqr_01");
-let element_02 = document.querySelector("#sqr_02");
-let element_10 = document.querySelector("#sqr_10");
-let element_11 = document.querySelector("#sqr_11");
-let element_12 = document.querySelector("#sqr_12");
-let element_20 = document.querySelector("#sqr_20");
-let element_21 = document.querySelector("#sqr_21");
-let element_22 = document.querySelector("#sqr_22");
+const endMessage = document.getElementsByTagName('h3')[0]
+endMessage.style.color = `red`
+endMessage.textContent = `X's turn!`
+endMessage.style.marginTop = '30px'
+endMessage.style.textAlign = 'center'
 
-function chance(element) {
-    if (player[0] === true) {
-        document.getElementById("player_0").style.color = `red`;
-        document.getElementById("player_1").style.color = `black`;
+let lock = 0;
 
-        element.innerHTML = `<i class="fa-solid fa-o fa-7x"></i>`
-        player[0] = false;
-        player[1] = true;
-        win_cond(player[0], player[1]);
-    } else if (player[1] === true) {
-        document.getElementById("player_0").style.color = `black`;
-        document.getElementById("player_1").style.color = `#63E6BE`;
-        element.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x"></i>`
-        player[0] = true;
-        player[1] = false;
-        win_cond(player[0], player[1]);
-    }
+let someoneWon = false;
+let win_cond = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("click", () => {
+        if (lock === 0) {
+            if (squares[i].innerHTML === ``) {
+
+                if (currentPlayer === player[0]) {
+                    squares[i].innerHTML = `<i class="fa-solid fa-xmark fa-7.5x"></i>`
+                    someoneWon = won()
+                    if (someoneWon === true) {
+                        if (currentPlayer === player[0]) {
+                            endMessage.style.color = `red`
+                            endMessage.textContent = `X Won!`
+                        } else {
+                            endMessage.style.color = `#63E6BE`
+                            endMessage.textContent = `0 Won!`
+                        }
+                        lock = 1;
+                        return
+                    }
+                    endMessage.style.color = `#63E6BE`
+                    endMessage.textContent = `0's turn!`
+                    currentPlayer = player[1]
+                }
+                else {
+                    squares[i].innerHTML = `<i class="fa-solid fa-o fa-7x"></i>`
+                    someoneWon = won()
+                    if (someoneWon === true) {
+                        if (currentPlayer === player[0]) {
+                            endMessage.style.color = `red`
+                            endMessage.textContent = `X Won!`
+                        } else {
+                            endMessage.style.color = `#63E6BE`
+                            endMessage.textContent = `0 Won!`
+                        } lock = 1;
+                        return
+                    }
+                    endMessage.style.color = `red`
+                    endMessage.textContent = `X's turn!`
+                    currentPlayer = player[0];
+                }
+            }
+        }
+
+
+
+    })
 }
-function sqrUpd_00() {
-    if (lock[0][0] === 0) {
-        if (flag === 0) {
-            flag = 1;
-            element_00.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x"></i>`
-            document.getElementById("player_0").style.color = `black`;
-            document.getElementById("player_1").style.color = `#63E6BE`;
-            player[0] = true;
-            lock[0][0] = 1;
+
+function won() {
+    for (let i = 0; i < 8; i++) {
+        const [a, b, c] = win_cond[i]
+        // console.log(win_cond[i])
+        // console.log(a, b, c)
+        if (currentPlayer === player[0]) {
+            if (squares[a].innerHTML === `<i class="fa-solid fa-xmark fa-7.5x"></i>`
+                && squares[b].innerHTML === `<i class="fa-solid fa-xmark fa-7.5x"></i>`
+                && squares[c].innerHTML === `<i class="fa-solid fa-xmark fa-7.5x"></i>`) {
+                squares[a].getElementsByTagName("i")[0].style.color = `red`
+                squares[b].getElementsByTagName("i")[0].style.color = `red`
+                squares[c].getElementsByTagName("i")[0].style.color = `red`
+                return true;
+
+            }
         }
         else {
-            lock[0][0] = 1;
-            chance(element_00)
+            if (squares[a].innerHTML === `<i class="fa-solid fa-o fa-7x"></i>`
+                && squares[b].innerHTML === `<i class="fa-solid fa-o fa-7x"></i>`
+                && squares[c].innerHTML === `<i class="fa-solid fa-o fa-7x"></i>`) {
+                squares[a].getElementsByTagName("i")[0].style.color = `#63E6BE`
+                squares[b].getElementsByTagName("i")[0].style.color = `#63E6BE`
+                squares[c].getElementsByTagName("i")[0].style.color = `#63E6BE`
+                return true;
+            }
         }
     }
-}
-function sqrUpd_01() {
-    if (lock[0][1] === 0) {
-        if (flag === 0) {
-            flag = 1;
-            element_01.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x"></i>`
-            document.getElementById("player_0").style.color = `black`;
-            document.getElementById("player_1").style.color = `#63E6BE`;
-            player[0] = true;
-            lock[0][1] = 1;
-        }
-        else {
-            lock[0][1] = 1;
-            chance(element_01)
-        }
-    }
+    // console.log("************** ")
 
-}
-function sqrUpd_02() {
-    if (lock[0][2] === 0) {
-        if (flag === 0) {
-            flag = 1;
-            element_02.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x"></i>`
-            document.getElementById("player_0").style.color = `black`;
-            document.getElementById("player_1").style.color = `#63E6BE`;
-            player[0] = true;
-            lock[0][2] = 1;
-        }
-        else {
-            lock[0][2] = 1;
-            chance(element_02)
-        }
-    }
-
-}
-function sqrUpd_10() {
-    if (lock[1][0] === 0) {
-        if (flag === 0) {
-            flag = 1;
-            element_10.innerHTML = `<i class="fa-solid fa-xmark fa-7x"></i>`
-            document.getElementById("player_0").style.color = `black`;
-            document.getElementById("player_1").style.color = `#63E6BE`;
-            player[0] = true;
-            lock[1][0] = 1;
-        }
-        else {
-            lock[1][0] = 1;
-            chance(element_10)
-        }
-    }
-}
-function sqrUpd_11() {
-    if (lock[1][1] === 0) {
-        if (flag === 0) {
-            flag = 1;
-            element_11.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x"></i>`
-            document.getElementById("player_0").style.color = `black`;
-            document.getElementById("player_1").style.color = `#63E6BE`;
-            player[0] = true;
-            lock[1][1] = 1;
-        }
-        else {
-            lock[1][1] = 1;
-            chance(element_11)
-        }
-    }
-
-}
-function sqrUpd_12() {
-    if (lock[1][2] === 0) {
-        if (flag === 0) {
-            flag = 1;
-            element_12.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x"></i>`
-            document.getElementById("player_0").style.color = `black`;
-            document.getElementById("player_1").style.color = `#63E6BE`;
-            player[0] = true;
-            lock[1][2] = 1;
-        }
-        else {
-            lock[1][2] = 1;
-            chance(element_12)
-        }
-    }
-
-}
-function sqrUpd_20() {
-    if (lock[2][0] === 0) {
-        if (flag === 0) {
-            flag = 1;
-            element_20.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x"></i>`
-            document.getElementById("player_0").style.color = `black`;
-            document.getElementById("player_1").style.color = `#63E6BE`;
-            player[0] = true;
-            lock[2][0] = 1;
-        }
-        else {
-            lock[2][0] = 1;
-            chance(element_20)
-
-        }
-    }
-
-}
-function sqrUpd_21() {
-    if (lock[2][1] === 0) {
-        if (flag === 0) {
-            flag = 1;
-            element_21.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x"></i>`
-            document.getElementById("player_0").style.color = `black`;
-            document.getElementById("player_1").style.color = `#63E6BE`;
-            player[0] = true;
-            lock[2][1] = 1;
-
-        }
-        else {
-            lock[2][1] = 1;
-            chance(element_21)
-        }
-    }
-
+    return false;
 }
 
-function sqrUpd_22() {
-    if (lock[2][2] === 0) {
-        if (flag === 0) {
-            flag = 1;
-            element_22.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x"></i>`
-            document.getElementById("player_0").style.color = `black`;
-            document.getElementById("player_1").style.color = `#63E6BE`;
-            player[0] = true;
-            lock[2][2] = 1;
-        }
-        else {
-            lock[2][2] = 1;
-            chance(element_22)
-        }
-    }
-}
-
-function isZero(element) {
-    const icon = element.querySelector("i");
-    return icon && icon.classList.contains("fa-o");
-}
-
-function isCross(element) {
-    const icon = element.querySelector("i");
-    return icon && icon.classList.contains("fa-xmark");
-}
-
-function win_cond(player_0, player_1) {
-    // Won by cross in 1st row
-    let c1r = wCross(element_00, element_01, element_02, player_0, player_1);
-
-    // Won by zero in 1st row
-    let z1r = wZero(element_00, element_01, element_02, player_0, player_1);
-
-    // Won by cross in 2nd row
-    let c2r = wCross(element_10, element_11, element_12, player_0, player_1);
-
-    // Won by zero in 2nd row
-    let z2r = wZero(element_10, element_11, element_12, player_0, player_1);
-
-    // Won by cross in 3nd row
-    let c3r = wCross(element_20, element_21, element_22, player_0, player_1);
-
-    // Won by zero in 3nd row
-    let z3r = wZero(element_20, element_21, element_22, player_0, player_1)
-
-    // Won by cross in 1st Column
-    let c1c = wCross(element_00, element_10, element_20, player_0, player_1);
-
-    // Won by zero in 1st row
-    let z1c = wZero(element_00, element_10, element_20, player_0, player_1);
-
-    // Won by cross in 2nd Column
-    let c2c = wCross(element_01, element_11, element_21, player_0, player_1);
-
-    // Won by zero in 2nd Column
-    let z2c = wZero(element_01, element_11, element_21, player_0, player_1);
-
-    // Won by cross in 3rd Column
-    let c3c = wCross(element_02, element_12, element_22, player_0, player_1);
-
-    // Won by zero in 3rd Column
-    let z3c = wZero(element_02, element_12, element_22, player_0, player_1);
-
-    // Won by Cross Main Diagonal `\`
-    let cmd = wCross(element_00, element_11, element_22, player_0, player_1);
-
-    // Won by Zero Main Diagonal `\`
-    let zmd = wZero(element_00, element_11, element_22, player_0, player_1);
-
-    // Won by Cross in Anti-Diagonal `/`
-    let cad = wCross(element_02, element_11, element_20, player_0, player_1);
-
-    // Won by Zero in Anti-Diagonal `/`
-    let zad = wZero(element_02, element_11, element_20, player_0, player_1);
-
-    // console.log("No Player Won? :", (c1r === 0 && z1r === 0 && c2r === 0 && z2r === 0 && c3r === 0 && z3r === 0 &&
-    //     c1c === 0 && z1c === 0 && c2c === 0 && z2c === 0 && c3c === 0 && z3c === 0 &&
-    //     cmd === 0 && zmd === 0 && cad === 0 && zad === 0))
-    // console.log("All Cells Locked? :", isAllLocked(lock))
-    if ((c1r === 0 && z1r === 0 && c2r === 0 && z2r === 0 && c3r === 0 && z3r === 0 &&
-        c1c === 0 && z1c === 0 && c2c === 0 && z2c === 0 && c3c === 0 && z3c === 0 &&
-        cmd === 0 && zmd === 0 && cad === 0 && zad === 0) && (isAllLocked(lock))) {
-        // No Player won And all the cells are filled
-        setTimeout(() => { alert("Its a Draw :|") }, 50)
-    }
-}
-
-function isAllLocked(lock) {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (lock[i][j] !== 1) return false;
+function cellFull() {
+    for (let i = 0; i < 9; i++) {
+        if (cells[i] !== 'X' || 'O') {
+            return false
         }
     }
     return true;
 }
 
-function wZero(element_a, element_b, element_c, player_0, player_1) {
-    if (isZero(element_a) && isZero(element_b) && isZero(element_c)) {
-        if ((player_0 === false) && (player_1 === true)) {
-            //player 1 won 
-            for (let i = 0; i < 3; i++) {// since a player won, game over
-                for (let j = 0; j < 3; j++) {
-                    lock[i][j] = 1
-                }
-            }
-            element_a.innerHTML = `<i class="fa-solid fa-o fa-7x" style="color: #63E6BE;"></i>`;
-            element_b.innerHTML = `<i class="fa-solid fa-o fa-7x" style="color: #63E6BE;"></i>`;
-            element_c.innerHTML = `<i class="fa-solid fa-o fa-7x" style="color: #63E6BE;"></i>`;
-            document.getElementById("player_0").style.color = `black`;
-            document.getElementById("player_1").style.color = `#63E6BE`;
-            setTimeout(() => { alert("Player 1 won, congratulations !!!") }, 50)
-            return 1
-        }
+function Draw() {
+    if (cellFull() && !won()) {
+        alert("Its Draww :|")
     }
-    else return 0;
-}
-
-function wCross(element_a, element_b, element_c, player_0, player_1) {
-    if (isCross(element_a) && isCross(element_b) && isCross(element_c)) {
-        if ((player_1 === false) && (player_0 === true)) {
-            //player 0 won 
-            for (let i = 0; i < 3; i++) {// since a player won, game over
-                for (let j = 0; j < 3; j++) {
-                    lock[i][j] = 1
-                }
-            }
-            element_a.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x" style="color: red;"></i>`
-            element_b.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x" style="color: red;"></i>`
-            element_c.innerHTML = `<i class="fa-solid fa-xmark fa-7.5x" style="color: red;"></i>`
-            document.getElementById("player_0").style.color = `red`;
-            document.getElementById("player_1").style.color = `black`;
-            setTimeout(() => { alert("Player 0 won, congratulations !!!") }, 50)
-            return 1
-        }
-    }
-    else return 0
-}
-
-function newGame() {
-    element_00.innerHTML = ``
-    element_01.innerHTML = ``
-    element_02.innerHTML = ``
-    element_10.innerHTML = ``
-    element_11.innerHTML = ``
-    element_12.innerHTML = ``
-    element_20.innerHTML = ``
-    element_21.innerHTML = ``
-    element_22.innerHTML = ``
-
-    lock = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ];
-
-    flag = 0;
-
-    document.getElementById("player_0").style.color = `red`;
-    document.getElementById("player_1").style.color = `black`;
 }
